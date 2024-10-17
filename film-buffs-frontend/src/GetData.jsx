9db4cb8c9aaa3f4c3d61fc5e359f6864e5c70703
Tyler <brown.tyler.w@gmail.com>
2024-10-16 21:39:51 -0700

    Search for movies by entering title in search bar. Displays title and release date

--- film-buffs-frontend/src/GetData.jsx
+++ film-buffs-frontend/src/GetData.jsx
@@ -3,46 +3,44 @@ import { useState } from "react";
 import { options } from "./options";
 
 export const GetData = () => {
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState('')
-    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState(null);
+    const [film, setFilm] = useState('');
+    const [filmDate, setFilmDate] = useState('');
 
+    // Whenever search is updated, searches for a movie using the value of search as a query
     useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/550?language=en-US', options)
-        .then(response => response.json())
-        .then(response => setData(response))
-        .catch(err => console.error(err))
-        .finally(setLoading(false));
-    }, []);
-
-    useEffect(() => {
+        if (search) {
         fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`, options)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => {
+            // Takes the retrieved title and release date and sets our states to them
+            setFilm(response.results[0].title);
+            setFilmDate(response.results[0].release_date);
+        })
         .catch(err => console.error(err));
-    }, []);
+        }
+    }, [search]);
 
+    // Retrieves the value from our search bar and parses it to be ready to insert into our API call URL
     const handleSubmit = (e) => {
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
         const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
-        setSearch(formJson.Title);
-        console.log(search)
+        // Encodes our search value to insert into URL: 'Fight Club' => 'fight%20club'
+        setSearch(encodeURIComponent(formJson.Title.toLowerCase()));
     }
     
     if (loading) return <h1>Loading...</h1>
     return (
         <>
-            <h1>{data.title}</h1>
+            <h1>{film}</h1>
+            <h1>{filmDate}</h1>
             <form onSubmit={handleSubmit}>
                 <input name="Title" type="text" placeholder="Enter Movie Title"/>
                 <button type="submit">Submit</button>
             </form>
-            <h1>{search}</h1>
         </>
     )
 }
-
-
