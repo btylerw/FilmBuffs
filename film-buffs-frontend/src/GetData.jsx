cff026b0ea98e161f4d1afd9f6ed15dc25fb5510
Tyler <brown.tyler.w@gmail.com>
2024-10-16 19:47:33 -0700

    Created form to search for movies by title. Need to implement functionality to parse data and add to api call

--- film-buffs-frontend/src/GetData.jsx
+++ film-buffs-frontend/src/GetData.jsx
@@ -1,16 +1,11 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { options } from "./options";
 
 export const GetData = () => {
     const [loading, setLoading] = useState(true);
-    const [data, setData] = useState('');
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer ' + `${import.meta.env.VITE_REACT_APP_API_KEY}`
-        }
-      };
+    const [data, setData] = useState('')
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetch('https://api.themoviedb.org/3/movie/550?language=en-US', options)
@@ -19,11 +14,33 @@ export const GetData = () => {
         .catch(err => console.error(err))
         .finally(setLoading(false));
     }, []);
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+        console.log(formJson);
+        setSearch(formJson.Title);
+        console.log(search)
+    }
     
     if (loading) return <h1>Loading...</h1>
     return (
         <>
             <h1>{data.title}</h1>
+            <form onSubmit={handleSubmit}>
+                <input name="Title" type="text" placeholder="Enter Movie Title"/>
+                <button type="submit">Submit</button>
+            </form>
+            <h1>{search}</h1>
         </>
     )
 }
